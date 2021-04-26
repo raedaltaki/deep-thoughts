3fae6181991016be1e2792acc9b65e4a98a72f68
raedaltaki <raed.simon@gmail.com>
2021-04-25 20:02:53 -0400

    add Auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,24 +3,8 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-
-    type Thought {
-        _id: ID
-        thoughtText: String
-        createdAt: String
-        username: String
-        reactionCount: Int
-        reactions: [Reaction]
-    }
-
-    type Reaction {
-        _id: ID
-        reactionBody: String
-        createdAt: String
-        username: String
-    }
-
-    type User {
+    type User 
+    {
         _id: ID
         username: String
         email: String
@@ -29,11 +13,46 @@ const typeDefs = gql`
         friends: [User]
     }
 
-    type Query {
+    type Thought 
+    {
+        _id: ID
+        thoughtText: String
+        createdAt: String
+        username: String
+        reactionCount: Int
+        reactions: [Reaction]
+    }
+
+    type Reaction 
+    {
+        _id: ID
+        reactionBody: String
+        createdAt: String
+        username: String
+    }
+
+    type Query 
+    {
+        me: User
         users: [User]
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
+    }
+
+    type Mutation 
+    {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
+    }
+
+    type Auth 
+    {
+        token: ID!
+        user: User
     }
 `;
 
